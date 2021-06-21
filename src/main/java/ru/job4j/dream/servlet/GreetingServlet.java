d508ea868167c027251f4a901796e35651efb251
VadimShein <shein.v94@mail.ru>
2021-06-22 05:28:03 +0600

    4.1. JSON формат. [#238554 #170121]

--- src/main/java/ru/job4j/dream/servlet/GreetingServlet.java
+++ src/main/java/ru/job4j/dream/servlet/GreetingServlet.java
@@ -7,8 +7,8 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-public class GreetingServlet extends HttpServlet {
 
+public class GreetingServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         resp.setContentType("text/plain");
@@ -16,7 +16,7 @@ public class GreetingServlet extends HttpServlet {
         String email = req.getParameter("email");
         resp.setHeader("Access-Control-Allow-Origin", "*");
         PrintWriter writer = new PrintWriter(resp.getOutputStream());
-        writer.println("Entered email: " + email);
+        writer.println("{\"email\" : \"" + email + "\"}");
         writer.flush();
     }
 }
